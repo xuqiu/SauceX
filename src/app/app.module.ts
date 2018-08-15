10a043f3a4120e9a6f8fe47183f3611bcb9b6ed0
yinzhennan <yinzhennan123@gmial.com>
2018-08-15 12:43:08 +0800

    bottle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { OnsenModule } from 'ngx-onsenui';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { TabbarComponent } from './tabbar/tabbar.component';
@@ -11,6 +12,7 @@ import { DeviceComponent } from './device/device.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ConfigComponent } from './config/config.component';
 import { SettingsComponent } from './settings/settings.component';
+import { BottleComponent } from './bottle/bottle.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { SettingsComponent } from './settings/settings.component';
     DeviceComponent,
     StatisticsComponent,
     ConfigComponent,
-    SettingsComponent
+    SettingsComponent,
+    BottleComponent
   ],
   imports: [
     BrowserModule,
@@ -36,3 +39,4 @@ import { SettingsComponent } from './settings/settings.component';
   ],
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
